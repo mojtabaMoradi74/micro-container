a7ee1e418778df31405224ddbfec50ce457d1dea
mojtaba <moradi.mojtaba2674@gmail.com>
2021-04-29 14:40:42 +0430

    start

---
+++ .history/src/App_20210327132149.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+// import { createBrowserHistory } from "history";
+import MicroFrontend from "./MicroFrontend";
+
+import "./App.css";
+
+// const defaultHistory = createBrowserHistory();
+
+const {
+  REACT_APP_DOGS_HOST: dogsHost,
+  REACT_APP_CATS_HOST: catsHost,
+} = process.env;
+
+function Header() {
+  return (
+    <div className="banner">
+      <h1 className="banner-title">&#128571; Cats and Dogs &#128021;</h1>
+      <h4>Random pics of cats and dogs</h4>
+    </div>
+  );
+}
+
+function Dogs({ history }) {
+  return <MicroFrontend history={history} host={dogsHost} name="Users" />;
+}
+
+function Cats({ history }) {
+  console.log({ history });
+  return <MicroFrontend history={history} host={catsHost} name="Cats" />;
+}
+
+function GreetingCat({ history }) {
+  return (
+    <div>
+      <Header />
+      <div className="home">
+        <MicroFrontend history={history} host={catsHost} name="Cats" />
+      </div>
+    </div>
+  );
+}
+
+function Home({ history }) {
+  const [input, setInput] = useState("");
+
+  const handleOnClick = () => {
+    history.push(`/cat/${input}`);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="home">
+        <input
+          placeholder="Insert a greeting"
+          defaultValue={input}
+          onBlur={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleOnClick}>Greet Me</button>
+      </div>
+
+      <div className="home">
+        <div className="content">
+          <div className="cat">
+            <Cats />
+          </div>
+          <div className="dog">
+            <Link to={'/User'}>user</Link>
+            <Dogs />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Link to={'/'}>back to home</Link>
+      <React.Fragment>
+        <Switch>
+          <Route path="/user" component={Dogs} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cat/:greeting" component={GreetingCat} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
+  );
+}
+
+export default App;
